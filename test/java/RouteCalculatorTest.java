3526d78af60ac2efe6c72ad728dace3f6d4f2e81
Ilya <ibogino@yandex.ru>
2019-09-14 16:33:14 +0300

    initial commit

---
+++ test/java/RouteCalculatorTest.java
@@ -0,0 +1,37 @@
+import core.Line;
+import core.Station;
+import junit.framework.TestCase;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RouteCalculatorTest extends TestCase {
+
+    List<Station> route;
+
+    @Override
+    protected void setUp() throws Exception
+    {
+        route = new ArrayList<>();
+
+        Line line1 = new Line(1,"Первая");
+        Line line2 = new Line(2,"Вторая");
+
+        route.add(new Station("Петровская", line1));
+        route.add(new Station("Арбузная", line1));
+        route.add(new Station("Морковная",line2));
+        route.add(new Station("Яблочная",line2));
+    }
+
+    public void testCalculateDuration()
+    {
+        double actual = RouteCalculator.calculateDuration(route);
+        double expected = 8.5;
+        assertEquals(expected,actual);
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+
+    }
+}
